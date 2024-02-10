6e12c76d76cc822d0a5457db098b7c78576a9ece
mehrnaztaghivand <m.taghivand@gmail.com>
2024-02-11 01:08:16 +0330

    first time postgres configuration

--- pipeline.py
+++ pipeline.py
@@ -4,3 +4,25 @@ print(sys.argv)
 day = sys.argv[1]
 print (f'Job finished successfully for day= {day}')  """
 
+services:
+  postgres:
+    image: postgres:13
+    environment:
+      POSTGRES_USER: airflow
+      POSTGRES_PASSWORD: airflow
+      POSTGRES_DB: airflow
+    volumes:
+      - postgres-db-volume:/var/lib/postgresql/data
+    healthcheck:
+      test: ["CMD", "pg_isready", "-U", "airflow"]
+      interval: 5s
+      retries: 5
+    restart: always
+
+docker run -it \
+    -e POSTGRES_USER= "root" \
+    -e POSTGRES_PASSWORD: "root" \
+    -e POSRTGRES_DB: "ny_taxi" \
+    -v ny_tXi_postgres_data:/var/lib/postgresql/data
+    -p 5432:5432\
+postgres:13
