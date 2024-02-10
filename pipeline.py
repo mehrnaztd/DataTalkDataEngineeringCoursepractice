1fa5c551b5146030ebd69a27253acafbd0771fe6
mehrnaztaghivand <m.taghivand@gmail.com>
2024-02-11 00:02:21 +0330

    first pipeline commented

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
-import pandas as pd 
+""" mport pandas as pd 
 import sys
 print(sys.argv)
 day = sys.argv[1]
-print (f'Job finished successfully for day= {day}')
+print (f'Job finished successfully for day= {day}')  """
+
